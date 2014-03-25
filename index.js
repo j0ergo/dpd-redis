f77c7297482fa827e4280aedac6c572b219d6361
Jörg Ossenkopp <joerg.ossenkopp@gmail.com>
2014-03-25 12:03:22 +0100

    publishing and setting

--- index.js
+++ index.js
@@ -26,12 +26,15 @@ Redis.prototype.handle = function ( ctx, next ) {
       next();
   } else {
     if (ctx.body.method == "set") {
+      console.log("redis set");
       client.set(ctx.body.key, ctx.body.value, function(error, res) {
         if (error) console.log("error " + error);
         ctx.done(error, res);
       });
     } else if (ctx.body.method == "publish") {
+      console.log("redis publish");
       client.publish(ctx.body.key, ctx.body.value);
+      ctx.done(null, "OK");
     } else {
       next();
     }
